9c3d59e410bdd37d1bfc30df5984d71d5cf21927
riversword <2694473261@qq.com>
2017-09-23 23:25:56 +0800

    添加footer内容，完善部分语法

--- build a wikipedia viewer2.0.js
+++ build a wikipedia viewer2.0.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  var searchContent=$(".search input[name='searchBox']").val();
   $("input[name='search']").mousedown(function(){
     $(this).removeClass('searchButton');
     $(this).addClass('buttonDown');
@@ -17,33 +18,26 @@ $(document).ready(function(){
     }
   });
 	$("input[name='search']").click(function(){
-    var sContent=$(".search input[name='searchBox']").val();
-    sContent=sContent.replace(/^\s+/g,'');
-    //console.log("内容是："+sContent+"测试");
-    if(sContent != ''){
-      $("div.logo").slideUp("slow");
-      $(".logo img").slideUp("slow");
-    };
-	  
-	  var getUrl='https://en.wikipedia.org/w/api.php?action=query&origin=*&generator=search&gsrsearch='+sContent+'&format=json&gsrnamespace=0&prop=pageimages|extracts&exintro=true&pilimit=10&pithumbsize=100&exsentences=2'; 
+
+	  $("div.logo").slideUp("slow");
+    $(".logo img").slideUp("slow");
+	  var getUrl='https://en.wikipedia.org/w/api.php?action=query&origin=*&generator=search&gsrsearch='+$(".search input[name='searchBox']").val() +'&format=json&gsrnamespace=0&prop=pageimages|extracts&exintro=true&pilimit=10&pithumbsize=100&exsentences=2'; 
   
     $.ajax({
   	url:getUrl,
   	type:"get",
   	success:function(data){
-  		// console.log(data);
-  		// console.log(data.query.pages);
   		var pages=data.query.pages;
       //清空页面
   		$("#content").empty();
   		//遍历json中的pages属性
-  		for(i in pages){
-  			console.log(pages[i].title);
+  		for(var i in pages){
+
         if(pages[i].thumbnail!=null){
           $('#content').append("<div class='resultItem'><h3><a href='http://en.wikipedia.org/wiki?curid="+pages[i].pageid+"' target='_blank'>"+pages[i].title+"</a></h3>"+"<img src='"+pages[i].thumbnail.source+ "'>"+pages[i].extract+"</div>");
         }else $('#content').append("<div class='resultItem'><h3><a href='http://en.wikipedia.org/wiki?curid="+pages[i].pageid+"' target='_blank'>"+pages[i].title+"</a></h3>"+pages[i].extract+"</div>");
   		}
-  		  
+     
   	}
     });
 

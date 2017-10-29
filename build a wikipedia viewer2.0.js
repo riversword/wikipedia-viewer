e4f79bb6b4c7cbffb31fdaa51aae0dcc538b91c5
riversword <2694473261@qq.com>
2017-10-29 18:21:57 +0800

    代码格式规范化

--- build a wikipedia viewer2.0.js
+++ build a wikipedia viewer2.0.js
@@ -1,5 +1,8 @@
+
 $(document).ready(function(){
   var searchContent=$(".search input[name='searchBox']").val();
+
+  //change the style of the button when click "Go"
   $("input[name='search']").mousedown(function(){
     $(this).removeClass('searchButton');
     $(this).addClass('buttonDown');
@@ -12,11 +15,14 @@ $(document).ready(function(){
      $(this).removeClass('buttonDown');
      $(this).addClass('searchButton');
   });
+
+
   $("input[name='searchBox']").bind('keypress',function(event){
     if(event.keyCode=='13'){
-        $("input[name='search']").click();
+        $("input[name='search']").click(); //type "center"==click "Go"
     }
   });
+
 	$("input[name='search']").click(function(){
 
 	  $("div.logo").slideUp("slow");
@@ -24,21 +30,21 @@ $(document).ready(function(){
 	  var getUrl='https://en.wikipedia.org/w/api.php?action=query&origin=*&generator=search&gsrsearch='+$(".search input[name='searchBox']").val() +'&format=json&gsrnamespace=0&prop=pageimages|extracts&exintro=true&pilimit=10&pithumbsize=100&exsentences=2'; 
   
     $.ajax({
-  	url:getUrl,
-  	type:"get",
-  	success:function(data){
-  		var pages=data.query.pages;
-      //清空页面
-  		$("#content").empty();
-  		//遍历json中的pages属性
-  		for(var i in pages){
-
-        if(pages[i].thumbnail!=null){
-          $('#content').append("<div class='resultItem'><h3><a href='http://en.wikipedia.org/wiki?curid="+pages[i].pageid+"' target='_blank'>"+pages[i].title+"</a></h3>"+"<img src='"+pages[i].thumbnail.source+ "'>"+pages[i].extract+"</div>");
-        }else $('#content').append("<div class='resultItem'><h3><a href='http://en.wikipedia.org/wiki?curid="+pages[i].pageid+"' target='_blank'>"+pages[i].title+"</a></h3>"+pages[i].extract+"</div>");
-  		}
+  	   url:getUrl,
+  	   type:"get",
+  	   success:function(data){
+  		            var pages=data.query.pages;
+                  //clear the content
+  		            $("#content").empty();
+                  
+  		            //get every  property of pages in data
+  		            for(var i in pages){
+                    if(pages[i].thumbnail!=null){
+                      $('#content').append("<div class='resultItem'><h3><a href='http://en.wikipedia.org/wiki?curid="+pages[i].pageid+"' target='_blank'>"+pages[i].title+"</a></h3>"+"<img src='"+pages[i].thumbnail.source+ "'>"+pages[i].extract+"</div>");
+                    }else $('#content').append("<div class='resultItem'><h3><a href='http://en.wikipedia.org/wiki?curid="+pages[i].pageid+"' target='_blank'>"+pages[i].title+"</a></h3>"+pages[i].extract+"</div>");
+  		            }
      
-  	}
+  	   }
     });
 
 	});
